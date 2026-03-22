6066643702b72f274651ca9e1bf38a8e0a83a90b
HouKunLin <houkunlin@aliyun.com>
2026-03-22 20:30:42 +0800

    refactor: 删除所有Java代码，重来一遍，新版本使用 1.9.1.0

--- src/main/java/com/scientiamobile/wurfl/core/web/introspector/c.java
+++
@@ -1,9 +0,0 @@
-package com.scientiamobile.wurfl.core.web.introspector;
-
-final class c {}
-
-
-/* Location:              D:\workspace\houkunlin\wurfl-jdk17\libs\wurfl-1.9.0.0.jar!\com\scientiamobile\wurfl\core\web\introspector\c.class
- * Java compiler version: 5 (49.0)
- * JD-Core Version:       1.1.3
- */
--- src/main/java/com/scientiamobile/wurfl/core/web/introspector/d.java
+++
@@ -1,9 +0,0 @@
-package com.scientiamobile.wurfl.core.web.introspector;
-
-final class d {}
-
-
-/* Location:              D:\workspace\houkunlin\wurfl-jdk17\libs\wurfl-1.9.0.0.jar!\com\scientiamobile\wurfl\core\web\introspector\d.class
- * Java compiler version: 5 (49.0)
- * JD-Core Version:       1.1.3
- */
