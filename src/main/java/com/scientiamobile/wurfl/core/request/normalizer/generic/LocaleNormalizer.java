eb4ec6f3fc1770c765ac698858bec43ce7d6a1f0
HouKunLin <houkunlin@aliyun.com>
2026-05-27 01:46:00 +0800

    fix: 移除 Pattern.COMMENTS

--- src/main/java/com/scientiamobile/wurfl/core/request/normalizer/generic/LocaleNormalizer.java
+++ src/main/java/com/scientiamobile/wurfl/core/request/normalizer/generic/LocaleNormalizer.java
@@ -2,10 +2,9 @@ package com.scientiamobile.wurfl.core.request.normalizer.generic;
 
 import com.scientiamobile.wurfl.core.request.normalizer.UserAgentNormalizer;
 import java.util.regex.Pattern;
-import static java.util.regex.Pattern.COMMENTS;
 
 public class LocaleNormalizer implements UserAgentNormalizer {
-   private static final Pattern LOCALE_PATTERN = Pattern.compile("; ?[a-z]{2}(?:-r?[a-zA-Z]{2})?(?:\\.utf8|\\.big5)?\\b-?(?!:)", Pattern.COMMENTS);
+   private static final Pattern LOCALE_PATTERN = Pattern.compile("; ?[a-z]{2}(?:-r?[a-zA-Z]{2})?(?:\\.utf8|\\.big5)?\\b-?(?!:)");
 
    @Override
    public String normalize(String userAgent) {
