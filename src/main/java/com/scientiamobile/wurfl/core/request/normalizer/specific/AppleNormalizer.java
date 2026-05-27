b66fa5daca44854ff2e9c118bee5d2e33945d229
HouKunLin <houkunlin@aliyun.com>
2026-05-27 15:50:36 +0800

    perf: 优化 AppleNormalizer.java

--- src/main/java/com/scientiamobile/wurfl/core/request/normalizer/specific/AppleNormalizer.java
+++ src/main/java/com/scientiamobile/wurfl/core/request/normalizer/specific/AppleNormalizer.java
@@ -41,29 +41,35 @@ public class AppleNormalizer implements UserAgentNormalizer {
 
         if (versionMatcher != null) {
             String iosVersion = versionMatcher.group(1).replace(".", "_");
-            StringBuilder normalizedUserAgent = new StringBuilder(256);
+            String prefix;
             if (userAgent.contains("iPad")) {
-                return normalizedUserAgent.append("Mozilla/5.0 (iPad; CPU OS ").append(iosVersion).append(" like Mac OS X) AppleWebKit/538.39.2 (KHTML, like Gecko) Version/7.0 Mobile/12A4297e Safari/9537.53 ").append(userAgent).toString();
+                prefix = "Mozilla/5.0 (iPad; CPU OS " + iosVersion + " like Mac OS X) AppleWebKit/538.39.2 (KHTML, like Gecko) Version/7.0 Mobile/12A4297e Safari/9537.53 ";
             } else if (userAgent.contains("iPod touch")) {
-                return normalizedUserAgent.append("Mozilla/5.0 (iPod touch; CPU iPhone OS ").append(iosVersion).append(" like Mac OS X) AppleWebKit/538.41 (KHTML, like Gecko) Version/7.0 Mobile/12A307 Safari/9537.53 ").append(userAgent).toString();
+                prefix = "Mozilla/5.0 (iPod touch; CPU iPhone OS " + iosVersion + " like Mac OS X) AppleWebKit/538.41 (KHTML, like Gecko) Version/7.0 Mobile/12A307 Safari/9537.53 ";
+            } else if (userAgent.contains("iPod")) {
+                prefix = "Mozilla/5.0 (iPod; CPU iPhone OS " + iosVersion + " like Mac OS X) AppleWebKit/538.41 (KHTML, like Gecko) Version/7.0 Mobile/12A307 Safari/9537.53 ";
             } else {
-                return userAgent.contains("iPod") ? normalizedUserAgent.append("Mozilla/5.0 (iPod; CPU iPhone OS ").append(iosVersion).append(" like Mac OS X) AppleWebKit/538.41 (KHTML, like Gecko) Version/7.0 Mobile/12A307 Safari/9537.53 ").append(userAgent).toString() : normalizedUserAgent.append("Mozilla/5.0 (iPhone; CPU iPhone OS ").append(iosVersion).append(" like Mac OS X) AppleWebKit/601.1.10 (KHTML, like Gecko) Version/8.0 Mobile/12E155 Safari/600.1.4 ").append(userAgent).toString();
+                prefix = "Mozilla/5.0 (iPhone; CPU iPhone OS " + iosVersion + " like Mac OS X) AppleWebKit/601.1.10 (KHTML, like Gecko) Version/8.0 Mobile/12E155 Safari/600.1.4 ";
             }
-        } else {
-            if (userAgent != null) {
-                Matcher iosClientSdkMatcher = IOS_CLIENT_SDK_PATTERN.matcher(userAgent);
-                if (iosClientSdkMatcher.matches()) {
-                    return iosClientSdkMatcher.group(1);
+            return prefix + userAgent;
+        }
+
+        if (userAgent != null) {
+            Matcher iosClientSdkMatcher = IOS_CLIENT_SDK_PATTERN.matcher(userAgent);
+            if (iosClientSdkMatcher.matches()) {
+                return iosClientSdkMatcher.group(1);
+            }
+            if (findMatcher(userAgent, CPU_IOS_PATTERN) != null) {
+                String rewrittenUserAgent = userAgent.contains("iPad")
+                    ? userAgent.replace("CPU iOS", "CPU OS")
+                    : userAgent.replace("CPU iOS", "CPU iPhone OS");
+                versionMatcher = findMatcher(rewrittenUserAgent, CPU_OS_LIKE_PATTERN);
+                if (versionMatcher != null) {
+                    String cpuOsLike = versionMatcher.group(1).replace(".", "_");
+                    return rewrittenUserAgent.replace(" U;", "").replaceAll("CPU(?: iPhone)? OS ([\\d\\.]+) like", cpuOsLike);
                 }
             }
-
-            String rewrittenUserAgent;
-            if (userAgent != null && findMatcher(userAgent, CPU_IOS_PATTERN) != null && (versionMatcher = findMatcher(rewrittenUserAgent = userAgent.contains("iPad") ? userAgent.replace("CPU iOS", "CPU OS") : userAgent.replace("CPU iOS", "CPU iPhone OS"), CPU_OS_LIKE_PATTERN)) != null) {
-                String cpuOsLike = versionMatcher.group(1).replace(".", "_");
-                return rewrittenUserAgent.replace(" U;", "").replaceAll("CPU(?: iPhone)? OS ([\\d\\.]+) like", cpuOsLike);
-            } else {
-                return userAgent;
-            }
         }
+        return userAgent;
     }
 }
